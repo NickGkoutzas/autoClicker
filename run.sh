9aaada8bfd02b9371004ad0f97b36b2bf3139110
NickGkoutzas <72218979+NickGkoutzas@users.noreply.github.com>
2022-02-19 16:06:10 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y;
-sudo apt install -y python3-pip && pip install selenium;cd autoClicker;
+sudo apt install -y python3-pip && pip install selenium;
 tar -xvf geckodriver-v0.30.0-linux64.tar.gz && sudo mv geckodriver /usr/local/bin;
 export PATH=$PATH:/usr/local/bin/geckodriver && sudo apt install -y firefox;clear;
