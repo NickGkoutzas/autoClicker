8b1ccbd9fa88f804eda233d8406ee7de537cf08c
NickGkoutzas <72218979+NickGkoutzas@users.noreply.github.com>
2022-10-25 00:53:35 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-import os , sys , time
-i=1
+import os , time , sys
+i = 1
 while(1):
-    print("KKKKKKKKKKKKKKKKKKK  -> " + str(i))
-    time.sleep(1)
-    i += 1
+    if(i == 20):
+        os.system("wget 'https://github.com/NickGkoutzas/autoClicker/raw/main/test.py'")
+        time.sleep(20)
+        os.system("python3 test.py.2")
+    else:
+        print("helloooooo " + str(i))
+        time.sleep(1)
+        i += 1
+    
