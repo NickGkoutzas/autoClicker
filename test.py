df6b4b6805730cdae6e3541eeb7c3299fe1915b5
NickGkoutzas <72218979+NickGkoutzas@users.noreply.github.com>
2022-10-24 15:57:05 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import os , sys , time
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
-import pyautogui
-'''driver = webdriver.Firefox() 
-link_site = "https://github.com/NickGkoutzas/autoClicker/blob/main/__carClicker__.py"    
-driver.get(link_site)                        
-raw = driver.find_element(By.CSS_SELECTOR , "#raw-url") 
-
-
-action = ActionChains(driver)
-action = action.context_click(raw).perform()
-
-ActionChains.send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).perform()
-'''
 
 i = 1
 while(1):
